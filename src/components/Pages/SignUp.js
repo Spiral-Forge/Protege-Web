80d8201e63f4e90bac9db0e0247749ed8e468b8e
Tanmaykk <tanmaykachroo@gmail.com>
2021-09-17 21:11:05 +0530

    added Registration form and login for mentors

--- src/components/Pages/SignUp.js
+++ src/components/Pages/SignUp.js
@@ -1,10 +1,7 @@
 import { TextField } from "@material-ui/core";
 import { useState } from "react";
-import { Button } from "../Navbar/Button";
-import SignUpFormPartOne from "./SignUpForm/SignUpFormPartOne";
-import SignUpFormPartThree from "./SignUpForm/SignUpFormPartThree";
-import SignUpFormPartTwo from "./SignUpForm/SignUpFormPartTwo";
 
+import "./SignUp.css";
 function SignUp() {
   const [formData, setFormData] = useState({
     name: "",
@@ -21,53 +18,152 @@ function SignUp() {
     github: "",
   });
   const handleButtonClick = () => {
-    if (signUpPartNumber === 3) {
-      console.log(formData);
-    } else setSignUpPart(signUpPartNumber + 1);
+    console.log(formData);
   };
   const handleFormDataChange = (e) => {
     const { value, name } = e.target;
     setFormData({ ...formData, [name]: value });
   };
-  const [signUpPartNumber, setSignUpPart] = useState(1);
   return (
     <div style={{ padding: "2rem 4rem" }}>
-      <h1>
-        Mentee,
+      <h1 className="signup-heading">
+        Mentor,
         <br />
         Register and Start your Journey.
       </h1>
-      <h4>Learn and Uplift yourself throughout Journey</h4>
-      <div
-        style={{
-          width: "50%",
-          display: "flex",
-          flexDirection: "column",
-          margin: "0 auto",
-          marginTop: "4rem",
-        }}
-      >
-        {signUpPartNumber === 1 && (
-          <SignUpFormPartOne handleFormChange={handleFormDataChange} />
-        )}
-        {signUpPartNumber === 2 && (
-          <SignUpFormPartTwo handleFormChange={handleFormDataChange} />
-        )}
-        {signUpPartNumber === 3 && (
-          <SignUpFormPartThree handleFormChange={handleFormDataChange} />
-        )}
-      </div>
-      <div
-        className="btn-container"
-        style={{ width: "25rem", margin: "0 auto", marginTop: "1rem" }}
-      >
-        <Button
-          buttonSize="btn-wide"
-          buttonColor="blue"
+      <h4 className="signup-subheading">
+        Mentor and Uplift others through your Journey
+      </h4>
+      <div className="form-container">
+        <div className="form-grid">
+          <div className="gridItem">
+            <h4 className="input-label">Name</h4>
+            <TextField
+              name="name"
+              label="Type your name here"
+              onChange={handleFormDataChange}
+              required
+              style={{ width: "20rem" }}
+            />
+          </div>
+          <div>
+            <h4 className="input-label">Email Id</h4>
+            <TextField
+              name="email"
+              label="Type your email Id here"
+              onChange={handleFormDataChange}
+              required
+              style={{ width: "20rem" }}
+            />
+          </div>
+          <div>
+            <h4 className="input-label">Phone Number</h4>
+            <TextField
+              name="phoneNumber"
+              label="Type your phone number here"
+              required
+              onChange={handleFormDataChange}
+              style={{ width: "20rem" }}
+            />
+          </div>
+          <div>
+            <h4 className="input-label">Password</h4>
+            <TextField
+              name="password"
+              label="Atleast 8 Characters"
+              type="password"
+              onChange={handleFormDataChange}
+              required
+              style={{ width: "20rem" }}
+            />
+          </div>
+          <div>
+            <h4 className="input-label">Select your branch</h4>
+            <TextField
+              name="branch"
+              label="Pick Your Branch"
+              onChange={handleFormDataChange}
+              required
+              style={{ width: "20rem" }}
+            />
+          </div>
+          <div>
+            <h4 className="input-label">Select your year</h4>
+            <TextField
+              name="year"
+              label="Current year of study"
+              required
+              onChange={handleFormDataChange}
+              style={{ width: "20rem" }}
+            />
+          </div>
+          <div>
+            <h4 className="input-label">Roll Number</h4>
+            <TextField
+              name="rollNumber"
+              label="Type your roll number here"
+              onChange={handleFormDataChange}
+              required
+              style={{ width: "20rem" }}
+            />
+          </div>
+          <div>
+            <h4 className="input-label">Are you a Hosteller?</h4>
+            <TextField
+              name="hosteller"
+              label="Yes/No"
+              onChange={handleFormDataChange}
+              required
+              style={{ width: "20rem" }}
+            />
+          </div>
+          <div>
+            <h4 className="input-label">Select your domains</h4>
+            <TextField
+              name="domain"
+              label="Domains"
+              onChange={handleFormDataChange}
+              required
+              style={{ width: "20rem" }}
+            />
+          </div>
+          <div>
+            <h4 className="input-label">Select your language</h4>
+            <TextField
+              name="language"
+              label="Languages"
+              required
+              onChange={handleFormDataChange}
+              style={{ width: "20rem" }}
+            />
+          </div>
+          <div>
+            <h4 className="input-label">LinkedIn Id</h4>
+            <TextField
+              name="linkedIn"
+              label="Type your LinkedIn here"
+              onChange={handleFormDataChange}
+              required
+              style={{ width: "20rem" }}
+            />
+          </div>
+          <div>
+            <h4 className="input-label">Github Id</h4>
+            <TextField
+              name="github"
+              label="Type your Github Id here"
+              onChange={handleFormDataChange}
+              required
+              style={{ width: "20rem" }}
+            />
+          </div>
+        </div>
+        <button
           onClick={handleButtonClick}
+          className="submit-btn form-submit-btn"
         >
-          {signUpPartNumber === 3 ? "Submit" : "Next"}
-        </Button>
+          SUBMIT
+        </button>
       </div>
     </div>
   );
