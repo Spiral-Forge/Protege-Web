cbd66b2ea1dc41e9ba12a81fa813f19bb6755fd7
devanshi <devanshi.katyal09@gmail.com>
2021-09-23 22:51:55 +0530

    added resources

--- src/components/Resources/resourcepage.js
+++ src/components/Resources/resourcepage.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import './Resource.css'
+import {db} from "../../firebase"
 import {
     useParams
   } from "react-router-dom";
@@ -7,6 +8,26 @@ import Box from "./resourcebox";
 function Resource() {
     let { id } = useParams();
    
+   let [info , setInfo] = useState([]);
+    // Start the fetch operation as soon as
+    // the page loads
+    window.addEventListener('load', () => {
+        Fetchdata();
+      });
+  
+    // Fetch the required data using the get() method
+    const Fetchdata = ()=>{
+        db.collection("Development").get().then((querySnapshot) => {
+             
+            // Loop through the data and store
+            // it in array to display
+            querySnapshot.forEach(element => {
+                var data = element.data();
+                setInfo(arr => [...arr , data]);
+                  
+            });
+        })
+    }
     function divclicked(event) {
         if (event.target.nodeName === "I") {
           event.target.classList.add("clicked", "inverted");
@@ -20,19 +41,19 @@ function Resource() {
             <h1 className="SResourceHeading">{id} resources</h1>
            
             {
-                // props.links.map((resource) =>{
-                //     return(<div>
-                //         <Box title ="abc" link="{resource.link}"/>
-                //         <div class="widget " onClick={divclicked}>
-                //             <div class="upvote thumbs">
-                //                 <i class="thumbs-icon thumbs-icon-up"></i>
-                //             </div>
-                //             <div class="downvote thumbs">
-                //                 <i class="thumbs-icon thumbs-icon-down"></i>
-                //             </div>
-                //             </div>
-                //         </div>);
-                // })
+                info.map((resource) =>{
+                    return(<div>
+                        <Box title ={resource.Title} link={resource.Link}/>
+                        <div class="widget " onClick={divclicked}>
+                            <div class="upvote thumbs">
+                                <i class="thumbs-icon thumbs-icon-up"></i>
+                            </div>
+                            <div class="downvote thumbs">
+                                <i class="thumbs-icon thumbs-icon-down"></i>
+                            </div>
+                            </div>
+                        </div>);
+                })
 
             }
             
