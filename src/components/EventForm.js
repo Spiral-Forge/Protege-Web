115a3ced20c1fc0bedabb0dc82e8c57b4f2099d4
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-10-16 22:30:35 +0530

    Add Event Form

---
+++ src/components/EventForm.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import styles from "../styles/EventForm.module.css";
+import { MdOutlineTextFields } from "react-icons/md";
+import { BsCalendarDay, BsCardImage, BsFillFileTextFill } from "react-icons/bs";
+import { BiTimeFive } from "react-icons/bi";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+export default function EventForm({ setShowModal }) {
+  const [date, setDate] = useState();
+  const [time, setTime] = useState();
+
+  return (
+    <form>
+      <div className={styles.inputs}>
+        <div className={styles.flex}>
+          <MdOutlineTextFields className={styles.icon} />
+          <input type="text" placeholder="Title" />
+        </div>
+        <div className={styles.flex}>
+          <BsCalendarDay className={styles.icon} />
+          <DatePicker
+            placeholderText="Date"
+            selected={date}
+            onChange={setDate}
+          />
+        </div>
+        <div className={styles.flex}>
+          <BiTimeFive className={styles.icon} />
+          <DatePicker
+            selected={time}
+            onChange={setTime}
+            showTimeSelect
+            showTimeSelectOnly
+            timeIntervals={30}
+            timeCaption="Time"
+            dateFormat="h:mm aa"
+            placeholderText="Time"
+          />
+        </div>
+        <div className={styles.flex}>
+          <FaMapMarkerAlt className={styles.icon} />
+          <input type="text" placeholder="Venue" />
+        </div>
+        <div className={styles.flex}>
+          <BsCardImage className={styles.icon} />
+          <input type="text" placeholder="Image Url" />
+        </div>
+        <div className={styles.flex}>
+          <BsFillFileTextFill className={styles.icon} />
+          <textarea rows="1" placeholder="Description" />
+        </div>
+      </div>
+      <div className={styles.cta}>
+        <button onClick={() => setShowModal(false)}>Cancle</button>
+        <button onClick={() => setShowModal(false)}>Add Event</button>
+      </div>
+    </form>
+  );
+}
--- src/components/Pages/Event.js
+++ src/components/Pages/Event.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import styles from "../../styles/Event.module.css";
 import EventCard from "./EventCard";
-
+import { GoPlus } from "react-icons/go";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@material-ui/core";
+import EventForm from "../EventForm";
 export default function Event() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className={styles.container}>
       {/* <div className={styles.heading}>
@@ -15,6 +27,29 @@ export default function Event() {
         <EventCard />
         <EventCard />
       </div>
+      <div className={styles.btn}>
+        <button onClick={() => setShowModal(true)}>
+          <GoPlus />
+        </button>
+      </div>
+      <Dialog
+        PaperProps={{
+          style: {
+            overflow: "visible",
+          },
+        }}
+        open={showModal}
+        onClose={() => setShowModal(false)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Add Event"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            <EventForm setShowModal={setShowModal} />
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
