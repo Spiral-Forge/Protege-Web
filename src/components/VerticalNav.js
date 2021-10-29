283a87c899b7cab6ddb31456c4c07803c776a48f
Tanmaykk <tanmaykachroo@gmail.com>
2021-10-29 20:24:45 +0530

    profile pic on vertical nav links to profile page

--- src/components/VerticalNav.js
+++ src/components/VerticalNav.js
@@ -3,15 +3,17 @@ import { FiLogOut } from "react-icons/fi";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillChatDotsFill, BsCalendarEventFill } from "react-icons/bs";
 import { FaSwatchbook } from "react-icons/fa";
-
+import { Link } from "react-router-dom";
 export default function VerticalNav() {
   return (
     <div className={styles.container}>
       <div className={styles.pic}>
-        <img
-          src="https://avatars.githubusercontent.com/u/44186440?v=4"
-          alt=""
-        />
+        <Link to="/profile">
+          <img
+            src="https://avatars.githubusercontent.com/u/44186440?v=4"
+            alt=""
+          />
+        </Link>
       </div>
       <div className={styles.icons}>
         <AiFillHome className={styles.icon} />
