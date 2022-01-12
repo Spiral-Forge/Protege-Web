94a984b65a4fd3c648cff240ef8eef6a29c50e88
Tanmaykk <tanmaykachroo@gmail.com>
2022-01-12 22:50:43 +0530

    Minor changes

--- src/components/Resources/Resource.js
+++ src/components/Resources/Resource.js
@@ -4,56 +4,6 @@ import { useEffect } from "react";
 import { db } from "../../firebase";
 import { useState } from "react";
 
-// export const resourceCategories = [
-//   {
-//     id: 1,
-//     img: "development.png",
-//     txt: "Development",
-//     url: "Development",
-//   },
-//   {
-//     id: 2,
-//     img: "ml.png",
-//     txt: "Machine Learning",
-//     url: "ML",
-//   },
-//   {
-//     id: 3,
-//     img: "blogs.png",
-//     txt: "Scholarships",
-//     url: "Scholarship",
-//   },
-//   {
-//     id: 4,
-//     img: "opensrc.png",
-//     txt: "Open Source",
-//     url: "OpenSource",
-//   },
-//   {
-//     id: 5,
-//     img: "competitive.jpg",
-//     txt: "Competitive Coding",
-//     url: "CompCoding",
-//   },
-//   {
-//     id: 6,
-//     img: "college.jpg",
-//     txt: "College Resources",
-//     url: "College",
-//   },
-//   {
-//     id: 7,
-//     img: "blogs.png",
-//     txt: "Blogs and Articles",
-//     url: "Blogs and Articles",
-//   },{
-//     id: 6,
-//     img: "other.png",
-//     txt: "Miscellaneous",
-//     url: "Miscellaneous",
-//   },
-// ];
-
 export default function Resource() {
   const tempCardsData = [];
   const [cardsData, setCardsData] = useState([]);
@@ -91,7 +41,10 @@ export function ResourceCard({ card }) {
   };
 
   return (
-    <div onClick={() => handleClick(card.url)} className={styles.card}>
+    <div
+      onClick={() => handleClick(card.categoryName.split(" ").join("-"))}
+      className={styles.card}
+    >
       <div className={styles.img}>
         <img src={card.categoryLogo} alt="" />
       </div>
