9fc8081d3a1f132d445aea2b5ff257280f0bbcd4
nitashadhingra <nitashadhingra612@gmail.com>
2022-02-14 21:44:45 +0530

    UI edits

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -19,68 +19,65 @@ export default function FeedbackList() {
   }, [])
   
   useEffect(async () => {
-    // console.log(currentUser)
-    // console.log("user data: ", userData)
-    // console.log("peers: ", myPeers)
+
     var today = new Date();
     console.log(today.getDate());
 
     if (today.getDate() >= 25) {
       setAccessibility(true);
-    }
-
-    let tempPeerArr = [];
-    let profilePicsObj = {};
-    for (const id of myPeers) {
-      let photoUrl = null;
-      try{
-        await db
-        .collection("users")
-        .doc(id)
-        .get()
-        .then((doc) => {
-          tempPeerArr.push({ name: doc.data().name, userID: id });
-          photoUrl = doc.data().photoUrl
-        });
-      } catch (e) {
-        console.log("Peer data error")
-        console.log(id)
-      }
-
-      let tempDownloadURL;
-      if(photoUrl){
+      let tempPeerArr = [];
+      let profilePicsObj = {};
+      for (const id of myPeers) {
+        let photoUrl = null;
         try {
-          tempDownloadURL = await ProfilePicStorageRef.child(id).getDownloadURL();
+          await db
+          .collection("users")
+          .doc(id)
+          .get()
+          .then((doc) => {
+            tempPeerArr.push({ name: doc.data().name, userID: id });
+            photoUrl = doc.data().photoUrl
+          });
+        } catch (e) {
+          console.log("Peer data error")
+          console.log(id)
+        }
+
+        let tempDownloadURL;
+        if(photoUrl){
+          try {
+            tempDownloadURL = await ProfilePicStorageRef.child(id).getDownloadURL();
+          } catch (err) {
+            console.log("Uploaded img error")
+          }
+        }
+        else {
+          tempDownloadURL =
+                "https://avatars.dicebear.com/api/micah/" + id + ".svg";
+        }
+        profilePicsObj[id] = tempDownloadURL;
+      }
+      setPeerData(tempPeerArr);
+      console.log("PEEER DATA:", tempPeerArr)
+
+      let ownProfilePic;
+      if(currentUser.photoUrl){
+        try {
+          ownProfilePic = await ProfilePicStorageRef.child(
+            currentUser.uid
+          ).getDownloadURL();
         } catch (err) {
-          console.log("Uploaded img error")
+          console.log("profile pic error")
         }
       }
       else {
-        tempDownloadURL =
-              "https://avatars.dicebear.com/api/micah/" + id + ".svg";
+          ownProfilePic =
+            "https://avatars.dicebear.com/api/micah/" + currentUser.uid + ".svg";
       }
-      profilePicsObj[id] = tempDownloadURL;
+      profilePicsObj[currentUser.uid] = ownProfilePic;
+      setProfilePics(profilePicsObj);
+      console.log(peerData);
     }
-    setPeerData(tempPeerArr);
-    console.log("PEEER DATA:", tempPeerArr)
-
-    let ownProfilePic;
-    if(currentUser.photoUrl){
-      try {
-        ownProfilePic = await ProfilePicStorageRef.child(
-          currentUser.uid
-        ).getDownloadURL();
-      } catch (err) {
-        console.log("profile pic error")
-      }
-    }
-    else {
-        ownProfilePic =
-          "https://avatars.dicebear.com/api/micah/" + currentUser.uid + ".svg";
-    }
-    profilePicsObj[currentUser.uid] = ownProfilePic;
-    setProfilePics(profilePicsObj);
-    console.log(peerData);
   }, []);
 
   return (
--- src/components/Pages/Feedback.js
+++
@@ -1,9 +0,0 @@
-function Feedback() {
-    return (
-      <div>
-        <h1>Feedback page</h1>
-      </div>
-    );
-  }
-  
-  export default Feedback;
