bc3f08c5564a4fc045cf0da9f68f44b23bbdc134
Samiksha-Garg <gargsamiksha0306@gmail.com>
2023-01-09 21:49:39 +0530

    corrected an error

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,21 +1,20 @@
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
-const ProtectedRoute = ({component: Component, ...rest}) => {
-    
-    const { currentUser } = useAuth();
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const { currentUser } = useAuth();
 
-    return (
-        <Route 
-            {...rest}
-            render={(props) => {
-                if(!currentUser){
-                    return <Redirect to="/signin"/>
-                }
-                return <Component { ...props } />
-            }}
-        />
-    )
-}
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (!currentUser) {
+          return <Redirect to="/signin" />;
+        }
+        return <Component {...props} />;
+      }}
+    />
+  );
+};
 
 export default ProtectedRoute;
